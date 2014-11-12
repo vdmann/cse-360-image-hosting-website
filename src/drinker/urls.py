25085c199db2d6c855192a52f6dafa911a18b0f2
Rupert Monkey <rupert@example.com>
2014-11-11 20:37:15 -0700

    first commit, problems with dragdrop user=models.ForeignKey(User)

--- src/drinker/urls.py
+++
@@ -1,5 +0,0 @@
-# from django.conf.urls import patterns, include, url
- 
-# urlpatterns = patterns('',
-#     url(r'^$', 'drinker.views.DraggingAndDropping', name='DraggingAndDropping'),
-# )
--- src/photo_albums/models.py
+++
@@ -1,1 +0,0 @@
-# empty models file is needed for templatetags to work
