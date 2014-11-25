89981c1450a2f581a953822dbea2479e5742434e
Rupert Monkey <rupert@example.com>
2014-11-24 23:27:59 -0700

    16th commit, more testing/scalability

--- src/drinker/tests.py
+++ src/drinker/tests.py
@@ -517,40 +517,40 @@ class LoginUserNotFound(unittest.TestCase):
 
 ################################################################################
 # USER NAVIGATION
-# -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+# # -*- coding: utf-8 -*-
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.support.ui import Select
+# from selenium.common.exceptions import NoSuchElementException
+# from selenium.common.exceptions import NoAlertPresentException
+# import unittest, time, re
 
-class LoginNavigationBarTest(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
-        self.base_url = "http://127.0.0.1:8000/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
+# class LoginNavigationBarTest(unittest.TestCase):
+#     def setUp(self):
+#         self.driver = webdriver.Firefox()
+#         self.driver.implicitly_wait(30)
+#         self.base_url = "http://127.0.0.1:8000/"
+#         self.verificationErrors = []
+#         self.accept_next_alert = True
     
-    def test_login_navigation_bar(self):
-        driver = self.driver
-        driver.get(self.base_url)
-        driver.find_element_by_xpath("//input[@value='Login']").click()
-        driver.find_element_by_id("id_username").clear()
-        driver.find_element_by_id("id_username").send_keys("dee-mann")
-        driver.find_element_by_id("id_password").clear()
-        driver.find_element_by_id("id_password").send_keys("123")
-        driver.find_element_by_css_selector("input[type=\"submit\"]").click()
-        driver.find_element_by_xpath("(//button[@type='button'])[2]").click()
-        driver.find_element_by_xpath("(//input[@name='redirect'])[2]").click()
-        driver.find_element_by_xpath("(//button[@type='button'])[2]").click()
-        driver.find_element_by_xpath("(//input[@name='redirect'])[3]").click()
-        driver.find_element_by_xpath("(//button[@type='button'])[2]").click()
-        driver.find_element_by_xpath("(//input[@name='redirect'])[3]").click()
-        driver.find_element_by_xpath("(//button[@type='button'])[2]").click()
-        driver.find_element_by_name("redirect").click()
+#     def test_login_navigation_bar(self):
+#         driver = self.driver
+#         driver.get(self.base_url)
+#         driver.find_element_by_xpath("//input[@value='Login']").click()
+#         driver.find_element_by_id("id_username").clear()
+#         driver.find_element_by_id("id_username").send_keys("dee-mann")
+#         driver.find_element_by_id("id_password").clear()
+#         driver.find_element_by_id("id_password").send_keys("123")
+#         driver.find_element_by_css_selector("input[type=\"submit\"]").click()
+#         driver.find_element_by_xpath("(//button[@type='button'])[2]").click()
+#         driver.find_element_by_xpath("(//input[@name='redirect'])[2]").click()
+        # driver.find_element_by_xpath("(//button[@type='button'])[2]").click()
+        # driver.find_element_by_xpath("(//input[@name='redirect'])[3]").click()
+        # driver.find_element_by_xpath("(//button[@type='button'])[2]").click()
+        # driver.find_element_by_xpath("(//input[@name='redirect'])[3]").click()
+        # driver.find_element_by_xpath("(//button[@type='button'])[2]").click()
+        # driver.find_element_by_name("redirect").click()
     
 #     def is_element_present(self, how, what):
 #         try: self.driver.find_element(by=how, value=what)
@@ -573,15 +573,80 @@ class LoginNavigationBarTest(unittest.TestCase):
 #             return alert_text
 #         finally: self.accept_next_alert = True
     
-    def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
+    # def tearDown(self):
+    #     self.driver.quit()
+    #     self.assertEqual([], self.verificationErrors)
 
 # if __name__ == "__main__":
 #     unittest.main()
 
 
 
+
+
+# # -*- coding: utf-8 -*-
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.support.ui import Select
+# from selenium.common.exceptions import NoSuchElementException
+# from selenium.common.exceptions import NoAlertPresentException
+# import unittest, time, re
+
+# class LoginUserNavigation(unittest.TestCase):
+#     def setUp(self):
+#         self.driver = webdriver.Firefox()
+#         self.driver.implicitly_wait(30)
+#         self.base_url = "http://127.0.0.1:8000/"
+#         self.verificationErrors = []
+#         self.accept_next_alert = True
+    
+#     def test_login_user_navigation(self):
+#         driver = self.driver
+#         driver.get(self.base_url)
+#         driver.find_element_by_xpath("//input[@value='Login']").click()
+#         driver.find_element_by_id("id_username").clear()
+#         driver.find_element_by_id("id_username").send_keys("dee-mann")
+#         driver.find_element_by_id("id_password").clear()
+#         driver.find_element_by_id("id_password").send_keys("123")
+#         driver.find_element_by_css_selector("input[type=\"submit\"]").click()
+#         driver.find_element_by_xpath("(//input[@name='redirect'])[3]").click()
+#         driver.find_element_by_xpath("(//input[@name='redirect'])[2]").click()
+#         driver.find_element_by_xpath("(//input[@name='redirect'])[3]").click()
+#         driver.find_element_by_xpath("(//input[@name='redirect'])[3]").click()
+#         driver.find_element_by_xpath("(//input[@name='redirect'])[2]").click()
+#         driver.find_element_by_xpath("(//input[@name='redirect'])[3]").click()
+#         driver.find_element_by_xpath("(//input[@name='redirect'])[2]").click()
+#         driver.find_element_by_xpath("(//input[@name='redirect'])[3]").click()
+#         driver.find_element_by_name("redirect").click()
+    
+#     def is_element_present(self, how, what):
+#         try: self.driver.find_element(by=how, value=what)
+#         except NoSuchElementException, e: return False
+#         return True
+    
+#     def is_alert_present(self):
+#         try: self.driver.switch_to_alert()
+#         except NoAlertPresentException, e: return False
+#         return True
+    
+#     def close_alert_and_get_its_text(self):
+#         try:
+#             alert = self.driver.switch_to_alert()
+#             alert_text = alert.text
+#             if self.accept_next_alert:
+#                 alert.accept()
+#             else:
+#                 alert.dismiss()
+#             return alert_text
+#         finally: self.accept_next_alert = True
+    
+    # def tearDown(self):
+    #     self.driver.quit()
+    #     self.assertEqual([], self.verificationErrors)
+
+# if __name__ == "__main__":
+#     unittest.main()
 
 
 # -*- coding: utf-8 -*-
@@ -593,7 +658,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
-class LoginUserNavigation(unittest.TestCase):
+class LoginUserNavigation2(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
@@ -606,40 +671,36 @@ class LoginUserNavigation(unittest.TestCase):
         driver.get(self.base_url)
         driver.find_element_by_xpath("//input[@value='Login']").click()
         driver.find_element_by_id("id_username").clear()
-        driver.find_element_by_id("id_username").send_keys("dee-mann")
+        driver.find_element_by_id("id_username").send_keys("123")
         driver.find_element_by_id("id_password").clear()
         driver.find_element_by_id("id_password").send_keys("123")
         driver.find_element_by_css_selector("input[type=\"submit\"]").click()
-        driver.find_element_by_xpath("(//input[@name='redirect'])[3]").click()
         driver.find_element_by_xpath("(//input[@name='redirect'])[2]").click()
-        driver.find_element_by_xpath("(//input[@name='redirect'])[3]").click()
-        driver.find_element_by_xpath("(//input[@name='redirect'])[3]").click()
         driver.find_element_by_xpath("(//input[@name='redirect'])[2]").click()
-        driver.find_element_by_xpath("(//input[@name='redirect'])[3]").click()
         driver.find_element_by_xpath("(//input[@name='redirect'])[2]").click()
-        driver.find_element_by_xpath("(//input[@name='redirect'])[3]").click()
+        driver.find_element_by_xpath("(//input[@name='redirect'])[2]").click()
         driver.find_element_by_name("redirect").click()
     
-#     def is_element_present(self, how, what):
-#         try: self.driver.find_element(by=how, value=what)
-#         except NoSuchElementException, e: return False
-#         return True
+    # def is_element_present(self, how, what):
+    #     try: self.driver.find_element(by=how, value=what)
+    #     except NoSuchElementException, e: return False
+    #     return True
     
-#     def is_alert_present(self):
-#         try: self.driver.switch_to_alert()
-#         except NoAlertPresentException, e: return False
-#         return True
+    # def is_alert_present(self):
+    #     try: self.driver.switch_to_alert()
+    #     except NoAlertPresentException, e: return False
+    #     return True
     
-#     def close_alert_and_get_its_text(self):
-#         try:
-#             alert = self.driver.switch_to_alert()
-#             alert_text = alert.text
-#             if self.accept_next_alert:
-#                 alert.accept()
-#             else:
-#                 alert.dismiss()
-#             return alert_text
-#         finally: self.accept_next_alert = True
+    # def close_alert_and_get_its_text(self):
+    #     try:
+    #         alert = self.driver.switch_to_alert()
+    #         alert_text = alert.text
+    #         if self.accept_next_alert:
+    #             alert.accept()
+    #         else:
+    #             alert.dismiss()
+    #         return alert_text
+    #     finally: self.accept_next_alert = True
     
     def tearDown(self):
         self.driver.quit()
@@ -648,6 +709,142 @@ class LoginUserNavigation(unittest.TestCase):
 # if __name__ == "__main__":
 #     unittest.main()
 
+
+
+
+
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class LoginUserNavigationBarTestUser(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://127.0.0.1:8000/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_login_user_navigation_bar_test_user(self):
+        driver = self.driver
+        driver.get(self.base_url)
+        driver.find_element_by_xpath("//input[@value='Login']").click()
+        driver.find_element_by_id("id_username").clear()
+        driver.find_element_by_id("id_username").send_keys("123")
+        driver.find_element_by_id("id_password").clear()
+        driver.find_element_by_id("id_password").send_keys("123")
+        driver.find_element_by_css_selector("button.navbar-toggle.collapsed").click()
+        driver.find_element_by_css_selector("input[type=\"submit\"]").click()
+        driver.find_element_by_xpath("(//button[@type='button'])[2]").click()
+        driver.find_element_by_xpath("(//input[@name='redirect'])[2]").click()
+        driver.find_element_by_xpath("(//button[@type='button'])[2]").click()
+        driver.find_element_by_xpath("(//input[@name='redirect'])[2]").click()
+        driver.find_element_by_xpath("(//button[@type='button'])[2]").click()
+        driver.find_element_by_xpath("(//input[@name='redirect'])[2]").click()
+        driver.find_element_by_xpath("(//button[@type='button'])[2]").click()
+        driver.find_element_by_xpath("(//input[@name='redirect'])[2]").click()
+        driver.find_element_by_xpath("(//button[@type='button'])[2]").click()
+        driver.find_element_by_name("redirect").click()
+    
+    # def is_element_present(self, how, what):
+    #     try: self.driver.find_element(by=how, value=what)
+    #     except NoSuchElementException, e: return False
+    #     return True
+    
+    # def is_alert_present(self):
+    #     try: self.driver.switch_to_alert()
+    #     except NoAlertPresentException, e: return False
+    #     return True
+    
+    # def close_alert_and_get_its_text(self):
+    #     try:
+    #         alert = self.driver.switch_to_alert()
+    #         alert_text = alert.text
+    #         if self.accept_next_alert:
+    #             alert.accept()
+    #         else:
+    #             alert.dismiss()
+    #         return alert_text
+    #     finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+# if __name__ == "__main__":
+#     unittest.main()
+
+
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class LoginUserNavigationBarTestAdmin(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://127.0.0.1:8000/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_login_user_navigation_bar_test_admin(self):
+        driver = self.driver
+        driver.get(self.base_url)
+        driver.find_element_by_xpath("//input[@value='Login']").click()
+        driver.find_element_by_css_selector("button.navbar-toggle.collapsed").click()
+        driver.find_element_by_id("id_username").clear()
+        driver.find_element_by_id("id_username").send_keys("dee-mann")
+        driver.find_element_by_id("id_password").clear()
+        driver.find_element_by_id("id_password").send_keys("123")
+        driver.find_element_by_css_selector("input[type=\"submit\"]").click()
+        driver.find_element_by_xpath("(//button[@type='button'])[2]").click()
+        driver.find_element_by_xpath("(//input[@name='redirect'])[2]").click()
+        driver.find_element_by_xpath("(//button[@type='button'])[2]").click()
+        driver.find_element_by_xpath("(//input[@name='redirect'])[2]").click()
+        driver.find_element_by_xpath("(//button[@type='button'])[2]").click()
+        driver.find_element_by_xpath("(//input[@name='redirect'])[2]").click()
+        driver.find_element_by_xpath("(//button[@type='button'])[2]").click()
+        driver.find_element_by_xpath("(//input[@name='redirect'])[2]").click()
+        driver.find_element_by_xpath("(//button[@type='button'])[2]").click()
+        driver.find_element_by_name("redirect").click()
+    
+    # def is_element_present(self, how, what):
+    #     try: self.driver.find_element(by=how, value=what)
+    #     except NoSuchElementException, e: return False
+    #     return True
+    
+    # def is_alert_present(self):
+    #     try: self.driver.switch_to_alert()
+    #     except NoAlertPresentException, e: return False
+    #     return True
+    
+    # def close_alert_and_get_its_text(self):
+    #     try:
+    #         alert = self.driver.switch_to_alert()
+    #         alert_text = alert.text
+    #         if self.accept_next_alert:
+    #             alert.accept()
+    #         else:
+    #             alert.dismiss()
+    #         return alert_text
+    #     finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+# if __name__ == "__main__":
+#     unittest.main()
 
 ################################################################################
 # UNIT TESTING HERE
